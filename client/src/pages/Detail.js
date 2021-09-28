f41e5a8ea9395493e740bfb389fac8ec8891ec33
Victor Lupascu <sinajeen@gmail.com>
2021-09-28 17:37:50 -0500

    add global state to add items to cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,8 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { useStoreContext } from '../utils/GlobalState'
-import { UPDATE_PRODUCTS } from '../utils/actions'
-
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions'
 import { QUERY_PRODUCTS } from '../utils/queries'
 import spinner from '../assets/spinner.gif'
 import Cart from '../components/Cart'
@@ -17,6 +21,12 @@ function Detail() {
   const { loading, data } = useQuery(QUERY_PRODUCTS)
 
   const { products } = state
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 },
+    })
+  }
 
   useEffect(() => {
     if (products.length) {
@@ -41,7 +51,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to cart</button>{' '}
             <button>Remove from Cart</button>
           </p>
 
