1304b6932a4ab7ea4b098355f1e10e487ecf3268
Victor Lupascu <sinajeen@gmail.com>
2021-09-28 13:10:00 -0500

    finish lesson 1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,13 @@
-import React, { useState } from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
+import ProductList from '../components/ProductList'
+import CategoryMenu from '../components/CategoryMenu'
 
 const Home = () => {
-  const [currentCategory, setCategory] = useState("");
-
   return (
     <div className="container">
-      <CategoryMenu setCategory={setCategory} />
-      <ProductList currentCategory={currentCategory} />
+      <CategoryMenu />
+      <ProductList />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
