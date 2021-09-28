daf9611a4880865630f0b56c54a1a58ef213c664
Victor Lupascu <sinajeen@gmail.com>
2021-09-28 12:23:21 -0500

    add the rest of reducers and tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react'
 import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
@@ -12,9 +13,21 @@ export const reducer = (state, action) => {
         ...state,
         products: [...action.products],
       }
-
-    // if it's none of these actions, do not update state at all and keep things the same!
+    // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      }
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      }
     default:
       return state
   }
+}
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState)
 }
