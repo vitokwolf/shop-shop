9bd0986eeca062420036d0f8aaf8e313e160a180
Victor Lupascu <sinajeen@gmail.com>
2021-09-30 11:46:14 -0500

    add indexDB to cart items

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CartItem from '../CartItem'
 import Auth from '../../utils/auth'
 import { useStoreContext } from '../../utils/GlobalState'
-import { TOGGLE_CART } from '../../utils/actions'
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions'
+import { idbPromise } from '../../utils/helpers'
 import './style.css'
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext()
 
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get')
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] })
+    }
+
+    if (!state.cart.length) {
+      getCart()
+    }
+  }, [state.cart.length, dispatch])
+
   function toggleCart() {
     dispatch({ type: TOGGLE_CART })
   }
+
   function calculateTotal() {
     let sum = 0
     state.cart.forEach((item) => {
@@ -18,6 +31,7 @@ const Cart = () => {
     })
     return sum.toFixed(2)
   }
+
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -27,6 +41,7 @@ const Cart = () => {
       </div>
     )
   }
+
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
